71cb6783624f79d840ea3fb3d321e93988b6b40a
Philip Forsting <Philip.Forsting@iths.se>
2025-09-25 01:04:43 +0200

    Finnished uppgift 1 and 2, but optimization is needed

--- Labb2.py
+++ Labb2.py
@@ -7,66 +7,100 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-dataPoints = []
-path_dataPoints = "../Python-programming-Labb2/datapoints.txt"
 
-with open(path_dataPoints, "r") as f_read:      # gör detta till en funktion istället
-    next(f_read)                                # Skip first row. Inspiration: https://stackoverflow.com/questions/4796764/read-file-from-line-2-or-skip-header-row
-    for line in f_read:
-        row = line.split(",")                   # row contains ["width", "height", "isPikachu"]
-        dataPoints.append([float(row[0]), float(row[1]), float(row[2])]) # converting elements in row to floats and adding them to list dataPoints Inspiration: https://stackoverflow.com/questions/21238242/python-read-file-into-2d-list
-f_read.close()
-dataPoints = np.array(dataPoints)               
+def ReadPointsFromFile(path, points, splitter):
+    with open(path, "r") as f_read:      
+        next(f_read)                                # Skip first row. Inspiration: https://stackoverflow.com/questions/4796764/read-file-from-line-2-or-skip-header-row
+        for line in f_read:
+            row = line.strip().split(splitter)                   
+            for i in range(len(row)):
+                row[i] = row[i].strip(".,()") 
+            points.append([float(row[0]), float(row[1]), float(row[2])]) # converting elements in row to floats and adding them to list Inspiration: https://stackoverflow.com/questions/21238242/python-read-file-into-2d-list
+    f_read.close()
+    return np.array(points)               
 
 
-# Grunduppgift: Plotta alla punkterna (varje klass får en färg) i samma fönster
-DataPoints_x = [line[0] for line in dataPoints] 
-DataPoints_y = [line[1] for line in dataPoints] 
-DataPoints_P = [line[2] for line in dataPoints] # for coloring each class separately 
-# Konvertera till Numpy så behövs inte koden ovan
+def ReadPointFromUser(testPoints):              # OPTIMERA DETTA
+    print("Enter a width and height of pokémon and program will classify it as Pikachu or Pichu")
+    while True:
+        try:
+            width = float(input("Enter width of pokémon: "))
+            if not 0 < width <= 1000:
+                print("Pokémon width must be between 0 and 1000 cm")
+                continue
+        except ValueError:
+            print("This is not a number")
+            continue
 
-plt.scatter(DataPoints_x,DataPoints_y, c=DataPoints_P) # colouring condition inspiration taken from: https://www.tutorialspoint.com/scatter-a-2d-numpy-array-in-matplotlib
-plt.legend(("Pichu", "Pikachu"))                # only first element is visible in legend. Are two scatters (for each class) needed?
+        try:
+            height = float(input("Enter height of pokémon: "))
+            if not 0 < height <= 1000:
+                print("Pokémon height must be between 0 and 1000 cm")
+                continue
+            testPoints.append([width, height])
+            return np.array(testPoints)
+        except ValueError:
+            print("This is not a number")
+            continue
+    
+
+
+def CalcDistAndClassify(testPoints, dataPoints, nrOfVoters):                # slå isär denna till CalcDist() och Classify()
+    testPointsZeroColumn = np.zeros((testPoints.shape[0], 1))                   # Prepairing to store classification result: Creating a new column of zeroes, same size of rows as data point array. 
+    testPoints = np.column_stack((testPoints, testPointsZeroColumn))              # Adding the new zero column to array d_Points
+    for testRow in testPoints:
+        vote_sum = 0
+        d_zeroColumn = np.zeros((dataPoints.shape[0], 1))                   # Prepairing to store classification result: Creating a new column of zeroes, same size of rows as data point array. 
+        dataPoints = np.column_stack((dataPoints, d_zeroColumn))              # Adding the new zero column to array d_Points Inspiration: https://medium.com/@heyamit10/numpy-add-column-guide-0427e394b333
+        testWidth = testRow[0]
+        testHeight = testRow[1]
+        for dataRow in dataPoints:
+            dataWidth = dataRow[0]
+            dataHeight = dataRow[1]
+            dataRow[-1] = np.sqrt((testWidth - dataWidth)**2 + (testHeight - dataHeight)**2)   # Storing distanse from test point to every datapoint in the new column  
+        dataPoints = dataPoints[np.argsort(dataPoints[:,-1])]                          # Sorting the d_Points array based on the distance value in new column. Inspiration: https://stackoverflow.com/questions/22698687/how-to-sort-2d-array-numpy-ndarray-based-to-the-second-column-in-python
+        for voter in range(nrOfVoters):
+            vote_sum += dataPoints[voter][2] 
+        if vote_sum == nrOfVoters/2.0:                                      # if voting result is a tie, radomize a class
+            vote_sum = np.random.randint(nrOfVoters)
+            print(f"Vote for sample with (width, height): ({testWidth}, {testHeight}) resulted in tie. Class has been randomized.")
+        if vote_sum >= nrOfVoters/2.0:
+            print(f"Sample with (width, height): ({testWidth}, {testHeight}) classified as Pikachu")
+            testRow[2] = 1
+        else:
+            print(f"Sample with (width, height): ({testWidth}, {testHeight}) classified as Pichu")
+            testRow[2] = 0
+ 
+
+#testPoints_x = [line[1] for line in testPoints] 
+#testPoints_y = [line[2] for line in testPoints] 
+#plt.scatter(testPoints_x,testPoints_y) 
 #plt.show()
-
-# Grunduppgift: Läs in testpunkter
-path_testpoints = "../Python-programming-Labb2/testpoints.txt"
-testPoints = []
-with open(path_testpoints, "r") as f_read:
-    next(f_read)                                # Skip first row. Inspiration: https://stackoverflow.com/questions/4796764/read-file-from-line-2-or-skip-header-row
-    for line in f_read:
-        row = line.strip().split(" ")
-        for i in range(len(row)):
-            row[i] = row[i].strip(".,()")       
-        testPoints.append((float(row[0]), float(row[1]), float(row[2])))
-f_read.close()
-
-testPoints_x = [line[1] for line in testPoints] 
-testPoints_y = [line[2] for line in testPoints] 
-plt.scatter(testPoints_x,testPoints_y) 
-plt.show()
 
 # Grunduppgift: Beräkna avstånd mellamn testpunkt och träningspunkter
 
 # Preparing to store distance between testpoint and datapoints in additinoal column
-zeroColumn = np.zeros((dataPoints.shape[0], 1)) # Creating a new row of zeroes, same size of rows as data point array. 
-dataPoints = np.column_stack((dataPoints, zeroColumn)) # Adding the new zero column. Inspiration: https://medium.com/@heyamit10/numpy-add-column-guide-0427e394b333
 
 # MAIN
-# dataPoints = ReadDataPointsFromFile()
-# PlotPoints(testPoints)
-# testPoints = ReadDataPointsFromFile()
-# testPoints = ReadDataPointsFromUser()
-# FindClosestPoint(dataPoints, testPoints, nrOfVoters)
 
+dataPoints = []     
+testPoints = []
+nrOfVoters = 10
+readTestPointsFromUser = True
+path_dataPoints = "../Python-programming-Labb2/datapoints.txt"
+path_testpoints = "../Python-programming-Labb2/testpoints.txt"
 
-for i in testPoints:
-    testWidth = i[1]
-    testHeight = i[2]
-    for j in dataPoints:
-        dataWidth = j[0]
-        dataHeight = j[1]
-        j[3] = np.sqrt((testWidth - dataWidth)**2 + (testHeight - dataHeight)**2) # Storing distanse from test point to every datapoint in the new column  
-    dataPoints = dataPoints[np.argsort(dataPoints[:,3])] # Sorting the dataPoints array based on the distance value in new column. Inspiration: https://stackoverflow.com/questions/22698687/how-to-sort-2d-array-numpy-ndarray-based-to-the-second-column-in-python
-    print(f"Sample with (width, height): ({testWidth}, {testHeight}) classified as Pikachu") if dataPoints[0][2] == 1 else print(f"Sample with (width, height): ({testWidth}, {testHeight}) classified as Pichu")
+dataPoints = ReadPointsFromFile(path_dataPoints, dataPoints, ",")   # After call, array dataPoints contains 3 columns ["width", "height", "isPikachu"]
+plt.scatter(dataPoints[:,0],dataPoints[:,1], c=dataPoints[:,2])     # colouring condition inspiration taken from: https://www.tutorialspoint.com/scatter-a-2d-numpy-array-in-matplotlib
+plt.legend(("Pichu", "Pikachu"))                                    # only "Pichu" is visible in legend. Are two scatters (for each class) needed?
+#plt.show()
+
+if readTestPointsFromUser:
+    testPoints = ReadPointFromUser(testPoints)                      # Se om denna kan optimeras
+else:
+    testPoints = ReadPointsFromFile(path_testpoints, testPoints, " ")   # After call, array testPoints contains 3 columns ["index" , "width", "height"]
+    testPoints = np.delete(testPoints, 0, 1)                            # Deleting index column from testPoint after reading from file. Arrays dataPoints and testPoints now both contain width and height in the first two columns. Inspiration:  https://stackoverflow.com/questions/64180609/delete-both-row-and-column-in-numpy-array
+CalcDistAndClassify(testPoints, dataPoints, nrOfVoters)             # After call, 
+
+       
 
